9700847b47d65a2ccaf2ce1b8480300a6af10213
pavan <pavankumarpk1923@gmail.com>
2026-07-31 11:39:34 +0530

    Solved Next Greater Element

---
+++ StriverA2Z/Stack_And_Queue/MonotonicStackAndQueues/NextGreaterElement.py
@@ -0,0 +1,13 @@
+def next_greater(nums:list)->list:
+    n = len(nums)
+    stack = []
+    ans = [-1]*n
+    for i in range(n):
+        while stack and nums[i] > nums[stack[-1]]:
+            idx = stack.pop()
+            ans[idx] = nums[i]
+        stack.append(i)
+    return ans
+
+nums = [2,1,4,3]
+print(next_greater(nums))
