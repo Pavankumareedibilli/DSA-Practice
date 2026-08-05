fc77fc5cfee39c4e525b755279afbc2b70cbcc99
Pavankumareedibilli <pavankumarpk1923@gmail.com>
2026-08-05 12:32:22 +0530

    Solved Sort Colors with optimal approch

---
+++ StriverA2Z/Array/Medium/sortColors.py
@@ -0,0 +1,19 @@
+def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        low = 0
+        mid = 0
+        high = n-1
+        
+        while mid<=high:
+            if nums[mid] == 0:
+                nums[mid],nums[low] = nums[low],nums[mid]
+                mid = mid +1
+                low = low +1
+            elif nums[mid] == 1:
+                mid = mid +1
+            else: 
+                nums[mid],nums[high] = nums[high],nums[mid]
+                high = high -1
